2cdbcdc93031bc873b89da9a1f51ebb3ab02421b
Abdu-Ak <abduak1425378@gmail.com>
2023-08-07 19:09:35 +0530

    google signin completed with graphql setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { NavLinks } from "@/constants";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUer } from "@/lib/session";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUer();
+
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -20,9 +22,17 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            userphoto
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                height={40}
+                width={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
             <Link href={"/create-project"}>share work</Link>
           </>
         ) : (
